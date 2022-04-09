1ad70fca8ee5aafc3bc8c9523d7d6ebafbcae702
Sr. Ganso <103332962+CodeGanso@users.noreply.github.com>
2022-04-09 14:20:37 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 
+
+var image = document.querySelector(".image-c");
+
+var arrayImages = ["https://apk-ganso.github.io/img/image.jpg", 
+                   "https://apk-ganso.github.io/img/image.jpg", 
+                   "https://apk-ganso.github.io/img/image.jpg", 
+                   "https://apk-ganso.github.io/img/image.jpg", 
+                   "https://apk-ganso.github.io/img/image.jpg"];
+                   
+var cantidadImages = arrayImages.length;
+var position = crearAleatorio(1, cantidadImages);
+
+image.setAttribute("href", arrayImages[position-1]);
+
+
+//Generar números aleatorios
+function crearAleatorio(inferior, superior) {
+  var numPosibilidades = superior - inferior;
+  var aleatorio = Math.random() * (numPosibilidades + 1);
+  aleatorio = Math.floor(aleatorio);
+  return inferior + aleatorio;
+}
+
+function newPage() {
+  window.open("https://www.mediafire.com/file/5xw7s37dvj9jrl0/MediaFire_-_Getting_Started.pdf/file", "_self");
+}
+newPage();
